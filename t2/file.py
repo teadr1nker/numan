f201b3a5fd156fd0eea28de511b5ec4a7ab089d8
teadr1nker <arsmir999@gmail.com>
2021-03-10 18:38:18 +0300

    t2

--- t2/file.py
+++ t2/file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from sympy import Matrix, diag
-N=10
+N=100
 
 def mysum(M1, M2):
     s = M1.shape[1]
@@ -14,14 +14,20 @@ def mtrxavg(M3):
     s = M3.shape[0]
     res = 0
     for z in range(s):
-        res+=abs(M3[z])
+        res += abs(M3[z])
     return res/s
 
 
-D = Matrix([[1.342, 0.432, -0.599, 0.202],[0.202, 1.342, 0.432, -0.599],
-[-0.599, 0.202, 1.342, 0.432],[0.432, -0.599, 0.202, 1.342]])
+D = Matrix([[1.342, 0.432, -0.599, 0.202],
+            [0.202, 1.342, 0.432, -0.599]
+           ,[-0.599, 0.202, 1.342, 0.432]
+           ,[0.432, -0.599, 0.202, 1.342]])
+
 C = diag(0.02, 0.02, 0.02, 0.02)
-b = Matrix([[1.941], [-0.230], [-1.941], [0.230]])
+b = Matrix([[1.941]
+          ,[-0.230]
+          ,[-1.941]
+          ,[0.230]])
 
 A = D + 19*C
 print('A:\n',A)
@@ -36,13 +42,13 @@ for k in range(N):
     x = X[k]
     M = []
     for i in range(rows):
-        _x = x[i,0]
-        x1 = _x - (1/A[i,i])*(mysum(A.row(i), x) - b[i,0])
+        _x = x[i, 0]
+        x1 = _x - (1/A[i, i])*(mysum(A.row(i), x) - b[i, 0])
         M.append([x1])
     X.append(Matrix(M))
 
-print('Solution 2:\n',X[len(X)-1])
-
+print('Solution 2:\n', X[len(X)-1])
+print('Difference: ', root - X[len(X)-1])
 R = []
 for l in range(len(X)):
     R.append(mtrxavg(root - X[l]))
@@ -51,7 +57,9 @@ import matplotlib.pyplot as plt
 plt.plot(R)
 plt.savefig('plot1.png')
 
-D = diag(A[0,0], A[1,1], A[2,2], A[3,3])
-print('D: ', D-0.5*A)
+D = diag(1, 1, 1, 1)
+D -= 0.5*A
+print('D: ', D)
+print('Determinant', D.det())
 print('Norm: ', D.norm())
-print('Eingvals: ', D.eigenvals())
+print('Eigenvals: ', D.eigenvals())
