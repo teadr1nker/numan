7a687f08f032f003e009668299d95eb5bda34777
teadr1nker <arsmir999@gmail.com>
2021-03-31 09:27:13 +0300

    t4

--- t4/file.py
+++ t4/file.py
@@ -1,3 +1,73 @@
 #!/usr/bin/python3
 
-print('Hello')
+import sympy as sp
+import numpy as np
+from functions import *
+from sympy.abc import x
+import matplotlib.pyplot as plt
+import math
+
+f = x ** 4 - sp.sqrt(x + 1) - 3
+a = 0.5
+b = 2.5
+j = 137
+T = []
+for r in range(j):
+    T.append(a + ((b-a)/j)*r)
+
+n = np.array([5, 10, 15, 20, 25, 30, 40, 50, 60])
+#print(f'T: {T}')
+N = n[0]
+X = []
+for i in range(N):
+    X.append(a + ((b-a)*i)/N)
+
+#L = sp.Sum(f.subs(x, X[k]) * sp.Product((t-X[i])/(X[k] - X[i]), (i, 0, N)), (k, 0, N))
+def L(t):
+    res = 0.0
+    for k in range(N):
+        mul = f.subs(x, X[k])
+        for i in range(N):
+            if X[k] - X[i] != 0:
+                mul *= (t - X[i]) / (X[k] - X[i])
+        res += mul
+    return res
+
+La = []
+for i in range(j):
+    La.append(L(T[i]))
+
+p1 = sp.plot(f, show=False)
+p1.save('plot1.png')
+
+plt.clf()
+plt.plot(La)
+plt.savefig('plot2.png')
+
+
+V = []
+for r in range(N):
+    V.append(math.cos(math.pi * ((1+2*r)/(2*(N+1)))))
+
+Z=[]
+for i in range(len(V)):
+    Z.append((a+b)/2 + ((b-a)/2)*V[i])
+
+def LCH(t):
+    res = 0.0
+    for k in range(N):
+        mul = f.subs(x, Z[k])
+        for i in range(N):
+            if Z[k] - Z[i] != 0:
+                mul *= (t - Z[i]) / (Z[k] - X[i])
+        res += mul
+    return res
+
+
+La = []
+for i in range(j):
+    La.append(LCH(T[i]))
+
+plt.clf()
+plt.plot(La)
+plt.savefig('plot3.png')
---
+++ t4/functions.py
@@ -0,0 +1,22 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def printbl(str):
+    print(f'{bcolors.OKBLUE}{str}{bcolors.ENDC}')
+
+def printh(str):
+    print(f'{bcolors.HEADER}{str}{bcolors.ENDC}')
+
+def printg(str):
+    print(f'{bcolors.OKGREEN}{str}{bcolors.ENDC}')
+
+def printbo(str):
+    print(f'{bcolors.BOLD}{str}{bcolors.ENDC}')
