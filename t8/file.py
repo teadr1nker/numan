5ea1692ffe2923cd6331b8dcbeefea7f98d97402
teadr1nker <arsmir999@gmail.com>
2021-04-30 18:57:31 +0300

    t8

---
+++ t8/file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def p(x):
+    return math.log(3.5 + x)
+
+def q(x):
+    return 0.8 * (x ** 2)
+
+def f(x):
+    return 2.5 * x - x ** 2
---
+++ t8/functions.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def tridiagonal(p, q, f, N, a, b, m1, m2):
+    h = (b - a) / N
+    X = np.linspace(_a, _b, N)
+    B = np.full(N, 1)
+    A = np.array([1 - p(x) * h for x in X])
+    C = np.array([2 - p(x) * h - q(x) * h for x in X])
+    G = np.array([-f(x) for x in X])
+    alpha = [0]
+    beta = [m1]
+    for i in N-1:
+        alpha.append(B[i+1]/(c[i+1] - a[i+1] * alpha[i]))
+        beta.append(((h**2)*G[i+1] + a[i+1]*beta[i]) / (C[i+1] - A[i+1]/alpha[i]))
+
+
+################################################################################
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+################################################################################
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def printbl(str):
+    print(f'{bcolors.OKBLUE}{str}{bcolors.ENDC}')
+
+def printh(str):
+    print(f'{bcolors.HEADER}{str}{bcolors.ENDC}')
+
+def printg(str):
+    print(f'{bcolors.OKGREEN}{str}{bcolors.ENDC}')
+
+def printbo(str):
+    print(f'{bcolors.BOLD}{str}{bcolors.ENDC}')
+
+import os
+def printLink(str):
+    os.system(f'echo \'\e]8;;file://{os.path.abspath(str)}\aOpen {str}\e]8;;\a\'')
